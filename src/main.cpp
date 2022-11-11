1f84891ab458710108f33845c9873d200cca56be
Atar Babgei <atarbabgei@gmail.com>
2022-11-11 12:52:52 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(PC13, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+  Serial.println("Hello World");
+}
